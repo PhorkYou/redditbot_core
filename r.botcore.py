a36a40ea0f727a234153a19167833f16bd3b932e
peterpacz1 <hong.shenzhou@gmail.com>
2014-01-05 16:18:36 +0800

    Started work on a better subreddit parser. Not done yet.

--- r.botcore.py
+++ r.botcore.py
@@ -1,8 +1,9 @@
 # Declaration of importation:
-import praw # Main praw api
-import math # Math libraries
-import time # Time libraries
-import os   # system command
+import praw                   # Main praw api
+import math                   # Math libraries
+import time                   # Time libraries
+import os                     # system command
+from collections import deque # deque lists
 
 # Clears the screen of the terminal
 os.system('cls' if os.name=='nt' else 'clear')
@@ -14,8 +15,9 @@ print "Communication between bot to reddit established"
 
 # Login to reddit function
 def login():
-    username = raw_input("Please enter username: ")
-    password = raw_input("Please enter password: ")
+    username = "bitcointripe"
+    print "Username: bitcointripe"
+    password = raw_input("Password: ")
     r.login(username, password)
 
 # Attempts to login
@@ -30,39 +32,28 @@ while logging:
         # Error handling
         print "Unable to login, please try again"
         
-# Asks user to choose between a single subreddit or submission, and returns the data
-def subreddit_or_submission():
+# Hotphrases list
+hotword = [
+    "testa"
+    "testb"
+    "testc"
+    "testd"
+    ]
+    
+# Active subreddits
+subreddit_list = "test"
+    
+# Banned subreddits:
+banned = [
+    "testban"
+    ]
 
-    # First asks the user to choose between a subreddit or submission
-    query = raw_input("Do you want to operate in a single submission or subreddit (submission/subreddit): ")
-    
-    # Specific hotwords 
-    hotwords = ["hotword1", "hotword2", "hotword3", "hotword4"]
-    
-    # List of comments already responded to
-    complete = set()
-    
-    if query == "submission":
-        submission_id = raw_input("Enter submission URL: ")
-        while True:
-            flat_comments = praw.helpers.flatten_tree(r.get_submission(submission_id).comments)
-            for comment in flat_comments:
-            
-                # First checks if the comment contains the hotwords, and if the comment is not already done
-                if comment.body in hotwords and comment.id not in complete:
-                    comment.reply("response")    # Replies
-                    complete.add(comment.id) # Adds the comment id to a list of already done comments
-                    print "cycle complete"
+# Already completed list (limit 300 to prevent memory leak)
+complete = deque(maxlen=300)
 
-    elif query == "subreddit":
-        subreddit_id = raw_input("Enter subreddit ID: ")
+def parse_comment():
+        subreddit = r.get_subreddit(subreddit_list)
+        sub_comments = subreddit.get_comments()
         
-        # Returns the data as subreddit variable
-        return subreddit
-        
-    else:
-    # If something breaks
-        print "Error: Invalid choice"
-        
-       
+parse_comment()
 print "All done"
